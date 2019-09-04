e5858f47708b74cfe07710e8482e93a00d95d490
Masaru Hirano <mhirano@neoblast.co.jp>
2019-09-04 14:44:08 +0900

    Add files via upload

---
+++ pencal/INSTALL.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ $(dpkg-query -W -f='${Status}' yad  2>/dev/null | grep -c "ok installed") -eq 1 ];
+then
+	echo "yad　Install 済み"
+
+else
+	sudo apt install -y yad
+fi
+
+if [ $(dpkg-query -W -f='${Status}' xinput-calibrator  2>/dev/null | grep -c "ok installed") -eq 1 ];
+then
+	echo "xinput-calibrator　Install 済み"
+
+else
+	sudo apt install -y xinput-calibrator
+fi
+
+
+#pencaldir
+dir=/opt/pentablet/pencal; [ ! -e $dir ] && sudo  mkdir -p $dir
+
+#penctrl.sh
+sudo cp  ./penctrl.sh  $dir
+
+#calmod.sh
+sudo cp  ./calmod.sh  $dir
+
+#xtcal
+sudo cp  ./xtcal  $dir
+
+#pencal.desktop
+sudo cp  ./pencal.desktop  /usr/share/applications/
+
+sudo chmod +x /usr/share/applications/pencal.desktop
+
+exit
